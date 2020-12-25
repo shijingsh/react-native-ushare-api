02e8ab224050cd42fde51d52efc0edeb828d9e6c
liukefu2050 <liukefu2050@sina.com>
2020-12-25 10:16:51 +0800

    init

--- android/src/main/java/com/mgUment/module/SharePackage.java
+++ android/src/main/java/com/mgUment/module/SharePackage.java
@@ -1,4 +1,4 @@
-package com.mg.app;
+package com.mgUment.module;
 
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.JavaScriptModule;
@@ -10,20 +10,21 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class PickerPackage implements ReactPackage {
+public class SharePackage implements ReactPackage {
+
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+        modules.add(new ShareModule(reactContext));
+        return modules;
+    }
 
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
 
+    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
-    }
-
-    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-        List<NativeModule> modules = new ArrayList<>();
-        modules.add(new PickerModule(reactContext));
-
-        return modules;
     }
 }
---
+++ android/src/main/java/com/mgUment/module/wxapi/WXEntryActivity.java
@@ -0,0 +1,16 @@
+package com.mgUment.wxapi;
+
+import com.tencent.mm.opensdk.constants.ConstantsAPI;
+import com.tencent.mm.opensdk.modelbase.BaseResp;
+import com.tencent.mm.opensdk.modelbiz.WXLaunchMiniProgram;
+import com.umeng.socialize.weixin.view.WXCallbackActivity;
+
+public class WXEntryActivity extends WXCallbackActivity {
+
+    public void onResp(BaseResp resp) {
+        if (resp.getType() == ConstantsAPI.COMMAND_LAUNCH_WX_MINIPROGRAM) {
+            WXLaunchMiniProgram.Resp launchMiniProResp = (WXLaunchMiniProgram.Resp) resp;
+            String extraData =launchMiniProResp.extMsg; //对应小程序组件 <button open-type="launchApp"> 中的 app-parameter 属性
+        }
+    }
+}
