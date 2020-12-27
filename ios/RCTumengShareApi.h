912d0207416b9a265ae51750fab677480c1e8504
liukefu <liukefu2020@sina.com>
2020-12-27 09:39:14 +0800

    2.1.3 ios

--- ios/RCTumengShareApi.h
+++ ios/RCTumengShareApi.h
@@ -1,3 +1,12 @@
+//
+//  ShareModule.h
+//  UMComponent
+//
+//  Created by wyq.Cloudayc on 11/09/2017.
+//  Copyright © 2017 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
 #import <React/RCTBridgeModule.h>
 
 @interface RCTumengShareApi : NSObject <RCTBridgeModule>
